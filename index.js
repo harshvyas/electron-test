b46f74ee78308617b54e4648414d5588d7331467
Harsh Vyas <harsh.vyas@gmail.com>
2017-09-30 22:42:32 -0400

    step2

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('hello')
+const { app , BrowserWindow } = require('electron')
+
+let mainWindow
+app.on('ready', () => {
+    const localFile = `file://` + __dirname + '/index.html' 
+    mainWindow = new BrowserWindow()
+    mainWindow.loadURL(localFile)
+})
